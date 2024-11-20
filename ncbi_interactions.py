d9078873b31eba0168fc509bba45808808236d39
Nicoisonline <doldinicolas@orange.fr>
2024-11-20 17:49:32 +0100

    :up: Update the way the functions work, add main functionnality to use it as a script

--- ncbi_interactions.py
+++ ncbi_interactions.py
@@ -1,5 +1,6 @@
 import csv
 import urllib.request
+import sys
 
 def download_file(url, filename):
 	"""Download the file from the given URL and save it locally"""
@@ -13,36 +14,56 @@ def download_summary_bacteria():
 	except:
 		print("Error downloading file : Bacteria summary")
 		
-def bacterie_download_data(taxname = None, projectID = None):
+def bacterie_download_data(input : str):
 	"""Return the correct format to download the file from ftp given the taxname or projectID"""
-	if taxname :
+	# We check if taxname start with "NC_"
+	isProjectId = 0
+	if input and input[:3] == "NC_":
+		isProjectId = 1
+	if isProjectId == 0:
 		with open("data/summary.txt", "r") as f:
 			reader = csv.reader(f, delimiter="\t")
 			for row in reader:
-				if taxname in row:
-					taxname_underscore = taxname.replace(" ", "_")
+				if input in row:
+					taxname_underscore = input.replace(" ", "_")
 					return taxname_underscore + "_uid" + row[4], row[0].split(".")[0]
 		print("Taxname not found in summary.txt")
-	if projectID:
+	else:
 		with open("data/summary.txt", "r") as f:
 			reader = csv.reader(f, delimiter="\t")
 			for row in reader:
-				if projectID in row:
+				if input in row:
 					taxname = row[5]
 					taxname_underscore = taxname.replace(" ", "_")
 					return taxname_underscore + "_uid" + row[4], row[0].split(".")[0]
 		print("Project ID not found in summary.txt")
 		
-def download_bacteria(taxname = None, projectID = None):
+def download_bacteria(input : str):
 	"""Download the file from ftp given the taxname or projectID"""
-	bacteria_dl_data = bacterie_download_data(taxname, projectID)
+	bacteria_dl_data = bacterie_download_data(input)
 	try:
 		download_file("ftp://ftp.ncbi.nih.gov/genomes/archive/old_refseq/Bacteria/" + bacteria_dl_data[0] + "/" + bacteria_dl_data[1] + ".faa", "data/protseq/" + bacteria_dl_data[1] + ".faa")
 		print("Protseq file downloaded successfully")
 	except:
-		print("Error downloading protseq file : Bacteria " + taxname)
+		print("Error downloading protseq file : Bacteria " + input)
 	try:
 		download_file("ftp://ftp.ncbi.nih.gov/genomes/archive/old_refseq/Bacteria/" + bacteria_dl_data[0] + "/" + bacteria_dl_data[1] + ".fna", "data/genomes/" + bacteria_dl_data[1] + ".fna")
 		print("Genome file downloaded successfully")
 	except:
-		print("Error downloading genomes file : Bacteria " + taxname)
+		print("Error downloading genomes file : Bacteria " + input)
+		
+def main():
+	args = sys.argv[1:]
+	if len(args) == 0:
+		print("Please provide the taxname or projectID")
+		return 0
+	else:
+		if args[0] == "summary":
+			print("Downloading bacteria summary")
+			download_summary_bacteria()
+		else:
+			print("Downloading bacteria data")
+			download_bacteria(args[0])
+
+if __name__ == "__main__":
+    main()
