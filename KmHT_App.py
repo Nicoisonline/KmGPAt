318490be66220c8b099701faab753020ca77c9e2
Nicoisonline <79758729+Nicoisonline@users.noreply.github.com>
2024-12-19 19:08:06 +0100

    :up: Add first options for parameters

--- KmHT_App.py
+++ KmHT_App.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 import ncbi_interactions as ncbi
 import threading
 import webbrowser
+import os
 
 class KmHT_App(ctk.CTk):
     def __init__(self, *args, **kwargs):
@@ -27,18 +28,44 @@ class KmHT_App(ctk.CTk):
 
         self.toplevel_download = None
 
+        self.label_kmer = ctk.CTkLabel(self.menu, text="Kmer size: ")
+        self.label_kmer.grid(row=3, column=0, sticky = "sw")
+
+        self.entry_kmer = ctk.CTkEntry(self.menu)
+        self.entry_kmer.grid(row=4, column=0, sticky = "sw")
+
+        self.bouton_kmer = ctk.CTkButton(self.menu, text="Compute", command=None)
+        self.bouton_kmer.grid(row=5, column=0, sticky = "sw")
         
+        self.file_1 = ctk.CTkLabel(self.menu, text="File 1: ")
+        self.file_1.grid(row=6, column=0, sticky = "sw")
+
+        self.files_menu()
+
 
         #Window
         self.windows_image = ctk.CTkFrame(self)
 
-        self.image = ctk.CTkImage(Image.open("output.png"), Image.open("output.png"), size=(578, 417))
-        self.label_image = ctk.CTkLabel(self.windows_image, image=self.image, text="")
-        self.label_image.grid(row=0, column=0)
+        try:
+            self.image = ctk.CTkImage(Image.open("output.png"), Image.open("output.png"), size=(578, 417))
+            self.label_image = ctk.CTkLabel(self.windows_image, image=self.image, text="")
+            self.label_image.grid(row=0, column=0)
+        except:
+            self.label_image = ctk.CTkLabel(self.windows_image, text="No image available")
+            self.label_image.grid(row=0, column=0)
 
         self.windows_image.grid(row=0, column=1, sticky = "")
 
         self.mainloop()
+
+    def files_menu(self):
+        self.file_1_combobox = ctk.CTkComboBox(self.menu, values=os.listdir("data/genomes"))
+        self.file_1_combobox.grid(row=7, column=0, sticky = "sw")
+
+        self.file_2_combobox = ctk.CTkComboBox(self.menu, values=os.listdir("data/genomes"))
+        self.file_2_combobox.grid(row=8, column=0, sticky = "sw")
+
+
 
     def open_download(self):
         if self.toplevel_download is None or not self.toplevel_download.winfo_exists():
@@ -94,18 +121,15 @@ class KmHT_Download(ctk.CTkToplevel):
         self.downloading = True
         self.input_dl = ctk.StringVar(value=self.entry.get())
         self.status.configure(text="Status: Downloading - This can take a while")
-        if self.input_dl.get() == "summary":
-            print("Downloading bacteria summary")
-            ncbi.download_summary_bacteria()
-            self.downloading = False
-        else:
-            print("Downloading bacteria data")
-            self.progressbar.start()
-            thread = threading.Thread(target=self.thread_dl)
-            thread.start()
+        self.progressbar.start()
+        thread = threading.Thread(target=self.thread_dl)
+        thread.start()
 
     def thread_dl(self):
-        res = ncbi.download_bacteria(str(self.input_dl.get()))
+        if self.input_dl.get() == "summary":
+            res = ncbi.download_summary_bacteria()
+        else:
+            res = ncbi.download_bacteria(str(self.input_dl.get()))
         if res == -1:
             self.status.configure(text="Status: Please provide the taxname or projectID")
         if res == 0:
